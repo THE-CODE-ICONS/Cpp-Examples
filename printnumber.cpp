9cac51887a68705f4db448501a9bb08d1f0fc03e
THE-CODE-ICONS <96696876+THE-CODE-ICONS@users.noreply.github.com>
2022-01-12 23:16:58 +0530

    Print Number Entered by User
    C++ Program to Print Number Entered by User
    This program asks the user to enter a number.
    When the user enters an integer, it is stored in variable number using cin >>  ;
    Then it is displayed on the screen using   cout << "      " ;

---
+++ printnumber.cpp
@@ -0,0 +1,13 @@
+//THE CODE ICONS
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int number ;
+
+    cout << "Enter an interger: " ;
+    cin >> number ;
+
+    cout << "you enterd " << number ;
+    return 0;
+}
