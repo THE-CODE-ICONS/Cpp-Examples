4ac485afc62eac832aad97160f64689d589f2d05
THE-CODE-ICONS <96696876+THE-CODE-ICONS@users.noreply.github.com>
2022-01-12 23:29:18 +0530

    Program to Add Two Integers
    C++ Program to Add Two Numbers
    In this program, user is asked to enter two integers. Then, the sum of those two integers is stored in a variable and displayed on the screen.

---
+++ addtwointegers.cpp
@@ -0,0 +1,17 @@
+//THE CODE ICONS 
+#include <iostream>
+using namespace std ;
+
+int main () {
+    int firstnumber , secondnumber , sumoftwonumber ;
+
+    cout << "Enter two intergers: " ;
+    cin >> firstnumber >> secondnumber ;
+
+    sumoftwonumber = firstnumber + secondnumber ;
+
+    cout << firstnumber << " + " << secondnumber << " = " << sumoftwonumber ;
+
+    return 0 ;
+
+}
